653163a2d534873927cccd7b2b8a0770d45384fb
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-12-21 15:56:00 -0800

    updated mod info and begining work on the script

---
+++ media/lua/client/SickMeter.lua
@@ -0,0 +1,19 @@
+--[[Made by Luna v0.0.1
+Im no expert in lua but i wanted this so i made it.
+]] --
+
+function MeterUpdate(character)
+    --[Variables]--
+    --[Damage taken]--
+    local bodyDamage = character:getBodyDamage()
+    --[Stats such as food,thirst etc]--
+    local playerStats = character:getStats()
+
+
+
+end
+
+function CheckMood(character)
+    
+end
+

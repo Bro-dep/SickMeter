8282f748d3706b0e3b88c22e8708bf7ea306d678
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-12-21 16:35:36 -0800

    Update SickMeter.lua

--- media/lua/client/SickMeter.lua
+++ media/lua/client/SickMeter.lua
@@ -1,8 +1,15 @@
---[[Made by Luna v0.0.1
+--[[
+========================================================
+Made by Luna 
 Im no expert in lua but i wanted this so i made it.
+========================================================
+Version: 0.0.1
+Date made: 12/21/2024
+Date modified: 12/21/2024
+========================================================
 ]] --
 
-function MeterUpdate(character)
+local function MeterUpdate(character)
     --[Variables]--
     --[Damage taken]--
     local bodyDamage = character:getBodyDamage()
@@ -13,7 +20,35 @@ function MeterUpdate(character)
 
 end
 
-function CheckMood(character)
+local function CheckMood(character)
     
 end
 
+local function Testing(character)
+    local bodyDamage = character:getBodyDamage()
+    local playerStats = character:getStats()
+    print(bodyDamage)
+    print(playerStats)
+end
+
+local function OnCreatePlayer(playerIndex, player)
+    if (playerIndex == 0) then
+        Testing(player)
+    end
+end
+
+local function OnPlayerUpdate(player)
+    Testing(player)
+    
+end
+
+local function OnKeyPressed(key)
+    if key == KEY_F1 then
+        print("F1 Pressed")
+    end
+end
+
+--[Events]--
+--Events.OnCreatePlayer.Add(OnCreatePlayer)
+--Events.OnPlayerUpdate.Add(OnPlayerUpdate)
+Events.OnKeyPressed.Add(OnKeyPressed)
